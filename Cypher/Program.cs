3f771a5c52f14607a206aafa54b8403f57e16b8a
Abraham Hosch <hosch250@live.com>
2022-02-19 20:20:00 -0600

    Build definitions

--- Cypher/Program.cs
+++ Cypher/Program.cs
@@ -81,4 +81,119 @@
 //    DIRECTED
 //}
 
-var x = 1;
+namespace Cypher
+{
+    using System.Linq.Expressions;
+    using static PersonToMovieRelationship;
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class LabelAttribute : Attribute
+    {
+        public LabelAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class RelationshipAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class KeyAttribute : Attribute { }
+
+    [Relationship]
+    public abstract record PersonToMovieRelationship
+    {
+        public record ACTED_IN(IReadOnlyList<string> Roles) : PersonToMovieRelationship { }
+        public record DIRECTED : PersonToMovieRelationship { }
+        public record WROTE : PersonToMovieRelationship { }
+        public record PRODUCED : PersonToMovieRelationship { }
+        public record REVIEWED(int Rating, string Summary) : PersonToMovieRelationship { }
+
+        public static ACTED_IN ActedIn(IReadOnlyList<string> Roles) => new(Roles);
+        public static DIRECTED Directed => new();
+        public static WROTE Wrote => new();
+        public static PRODUCED Produced => new();
+        public static REVIEWED Reviewed(int Rating, string Summary) => new(Rating, Summary);
+    }
+
+    [Label("Movie")]
+    public class Movie
+    {
+        [Key]
+        public string Title { get; set; }
+        public string Tagline { get; set; }
+        public int Released { get; set; }
+
+        public IEnumerable<(ACTED_IN relationship, Person person)> Actors { get; }  // we want to know the role the actor had
+        public IEnumerable<Person> DirectedBy { get; }                 // this relationship doesn't have roles, we just want the node
+        public IEnumerable<Person> WrittenBy { get; }
+        public IEnumerable<Person> ProducedBy { get; }
+        public IEnumerable<(REVIEWED relationship, Person person)> ReviewedBy { get; }
+    }
+
+    [Label("Person")]
+    public class Person
+    {
+        [Key]
+        public string Name { get; set; }
+        public int? Born { get; set; }
+
+        public IEnumerable<(ACTED_IN relationship, Movie movie)> ActedIn { get; }
+        public IEnumerable<Movie> Directed { get; }
+        public IEnumerable<Movie> Wrote { get; }
+        public IEnumerable<Movie> Produced { get; }
+        public IEnumerable<(REVIEWED relationship, Movie movie)> Reviewed { get; }
+    }
+
+    public interface IDatabase { }
+    public interface INodeSet<T>
+    {
+        // this will perform the following actions:
+        // 1. find node label; this will be `typeof(T)`, find `Label` attribute, use `Name` property: OR use type name if no `Label` attribute
+        // 2. evaluate expression into json match; todo: create/apply attribute to override property name
+        // 3. sanitize match expression, if necessary
+        // 4. build and run query expression
+        // 5. deserialize and return response
+        T Find(Expression<Func<T, bool>> expression);
+
+        // this will perform the following actions:
+        // 1. if an expression is provided
+        //    1. find node label; this will be `typeof(T)`, find `Label` attribute, use `Name` property: OR use type name if no `Label` attribute
+        //    2. evaluate expression into json match; todo: create/apply attribute to override property name
+        //    3. sanitize match expression, if necessary
+        // 2. build and run query expression
+        // 3. deserialize and return response
+        IEnumerable<T> FindAll(Expression<Func<T, bool>>? expression = null);
+
+        // this will perform the following actions:
+        // 1. if an expression is provided
+        //    1. find node label; this will be `typeof(T)`, find `Label` attribute, use `Name` property: OR use type name if no `Label` attribute
+        //    2. evaluate expression into json match; todo: create/apply attribute to override property name
+        //    3. sanitize match expression, if necessary
+        // 2. build and run query expression
+        // 3. deserialize and return response
+        int Count(Expression<Func<T, bool>>? expression = null);
+    }
+
+    public class Database : IDatabase
+    {
+        public INodeSet<Person> Persons { get; }
+        public INodeSet<Movie> Movies { get; }
+    }
+
+    public class Entry
+    {
+        public void Main() {
+            Database database = null!;
+            var person = database.Persons.Find(a => a.Name == "Tom Cruise");  // runs cypher `match (n:Person { name: "Tom Cruise" }) return n limit 1`
+            var actedIn = person.ActedIn;                                     // runs cypher `match (:Person { name: "Tom Cruise" })-[r:ACTED_IN]->(m) return r, m`
+            var movies = person.ActedIn.Select(s => s.movie);                 // runs cypher `match (:Person { name: "Tom Cruise" })-[:ACTED_IN]->(m:Movie) return m`
+            var movieRoles = person.ActedIn.First().relationship.Roles;       // runs cypher `match (:Person { name: "Tom Cruise" })-[r:ACTED_IN]->(:Movie) return r.roles limit 1`
+
+            var actedInMovieCount = database.Persons.Count();                 // runs cypher `match (n:Person) return count(n)`
+        }
+    }
+}
